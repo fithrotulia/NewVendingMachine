c9dfa39a2e59ba900ec52129df2ce0502994c8ae
fithrotulia <amalia.irda@gmail.com>
2019-12-01 18:51:42 +0700

    membuat projek vending machine sederhana

---
+++ Inc/MySeOS.h
@@ -0,0 +1,15 @@
+/*
+ * MySeOS.h
+ *
+ *  Created on: Nov 30, 2019
+ *      Author: fithrotul
+ */
+
+#ifndef INC_MYSEOS_H_
+#define INC_MYSEOS_H_
+
+void TIM4_IRQHandler(void);
+void SCH_Init(void);
+
+
+#endif /* INC_MYSEOS_H_ */
---
+++ Inc/MyUART.h
@@ -0,0 +1,20 @@
+/*
+ * MyUART.h
+ *
+ *  Created on: Nov 30, 2019
+ *      Author: fithrotul
+ */
+
+#ifndef INC_MYUART_H_
+#define INC_MYUART_H_
+
+#include "string.h"
+#include "stdio.h"
+
+void UART_Print(char *);
+void UART_Println( char *);
+void UART_PrintNum(uint16_t);
+
+
+
+#endif /* INC_MYUART_H_ */
